5352969388a697e94f9b7eadf5d22ae905c85b6f
Ipsis <Ipsis@abaddon>
2013-10-06 16:55:23 +0100

    Initial base files to allow the mod to load

---
+++ common/ipsis/mackit/MacKit.java
@@ -0,0 +1,28 @@
+package ipsis.mackit;
+
+import ipsis.mackit.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION_NUMBER)
+public class MacKit {
+
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
+	}
+}
---
+++ common/ipsis/mackit/lib/Reference.java
@@ -0,0 +1,8 @@
+package ipsis.mackit.lib;
+
+public class Reference {
+
+	public static final String MOD_ID = "mackit";
+	public static final String MOD_NAME = "Mining And Construction Kit";
+	public static final String VERSION_NUMBER = "0.0.1";
+}
