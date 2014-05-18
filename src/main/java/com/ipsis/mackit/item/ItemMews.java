10a756beacd8ce9e8cc693fc18346f253c4a41c3
Ipsis <ipsis418@gmail.com>
2014-05-18 15:17:02 +0100

    First pass at the Minecraft Early Warning System

---
+++ src/main/java/com/ipsis/mackit/item/ItemMews.java
@@ -0,0 +1,53 @@
+package com.ipsis.mackit.item;
+
+import java.util.Iterator;
+import java.util.List;
+
+import com.ipsis.mackit.helper.LogHelper;
+import com.ipsis.mackit.helper.Point;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.World;
+
+public class ItemMews extends ItemMK {
+
+	public ItemMews() {
+		
+		super();
+		this.setMaxStackSize(1);
+		this.setMaxDamage(FREQUENCY);
+	}
+	
+	private static final int SCAN_RANGE = 9;
+	private static final int FREQUENCY = 20;
+	
+	@Override
+	public void onUpdate(ItemStack itemStack, World world, Entity entity, int slot, boolean isHeld) {
+		
+		boolean checkArea = false;
+		
+		if (this.getDamage(itemStack) == FREQUENCY) {
+			this.setDamage(itemStack, 0);
+			checkArea = true;
+		} else {
+			this.setDamage(itemStack, this.getDamage(itemStack) + 1);
+		}
+		
+		if (checkArea && !world.isRemote) {
+			
+			double x = entity.posX;
+			double y = entity.posY;
+			double z = entity.posZ;
+					
+			List<EntityMob> e = world.getEntitiesWithinAABB(EntityMob.class, 
+								AxisAlignedBB.getBoundingBox(x - SCAN_RANGE, y - SCAN_RANGE, z - SCAN_RANGE, x + SCAN_RANGE, y + SCAN_RANGE, z + SCAN_RANGE));
+			
+			if (!e.isEmpty())	
+				world.playSoundAtEntity(entity, "dig.grass", 100, 10);									
+		}
+	}
+}
