383c8fa2df8772d8842336906f807b39f9862975
Ipsis <ipsis418@gmail.com>
2014-06-21 21:08:48 +0100

    Fixed earth should drop its item form when broken.

--- src/main/java/com/ipsis/mackit/block/BlockFixedEarth.java
+++ src/main/java/com/ipsis/mackit/block/BlockFixedEarth.java
@@ -2,21 +2,27 @@ package com.ipsis.mackit.block;
 
 import com.ipsis.mackit.reference.Reference;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.world.World;
 
-/**
- * Fixed earth cannot be collected
- * Destroys itself when broken.
- */
+import java.util.Random;
+
 public class BlockFixedEarth extends BlockMK {
 	
 	public BlockFixedEarth(Material material, String blockName) {
 		
 		super(material);		
 		this.setBlockName(blockName);
+        this.setHardness(0.5F);
 	}
-	
+
+    @Override
+    public int quantityDropped(Random rand) {
+
+        return 0;
+    }
 }
